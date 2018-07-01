8044274a3d669532707f21610e0210893a0e4895
Reioum25 <rhadj25@gmail.com>
2018-07-01 20:41:04 +0800

    Activity #2 - Rocket Mouse

---
+++ RocketMouse/Assets/RW/Scripts/MouseController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour 
+{
+	public float JetPackForce = 75.0f;
+	private Rigidbody2D PlayerRigidBody;
+
+	// Use this for initialization
+	void Start () 
+	{
+		PlayerRigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate () 
+	{
+		bool jetPackActive = Input.GetButton("Fire1");
+		if (jetPackActive)
+		{
+			PlayerRigidBody.AddForce(new Vector2(0, JetPackForce));
+		}
+	}
+}
